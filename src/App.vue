7fc8f830205dce1c1515ff97a82a99e8ac5c0e30
“Joe <joseph.sherman@Loop.colum.edu>
2018-12-11 21:29:06 -0600

    Add banner and popular sections from theme to home.vue. Updated text in banner to apply to Calamari Safari.

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
             </nav>
             <!-- /.navbar -->
         </header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">      <a class="navbar-brand" href="#">Calamari Safari</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">      <a class="navbar-brand" href="#">Calamari Safari</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>    
